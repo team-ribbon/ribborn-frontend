bd6d24aaddd28dee958e9746690b5de1e97717d2
treblabin <treblabin@gmail.com>
2022-07-06 16:30:53 +0900

    REFATOR: 지역 카테고리 파일 분리
    - 지역 카테고리 shared 폴더에 분리

--- src/components/SignupTech.js
+++ src/components/SignupTech.js
@@ -6,6 +6,7 @@ import styled from "styled-components";
 
 import { signupDB, usernameCheckDB } from "../redux/modules/user";
 import SignupAgree from "./SignupAgree";
+import { AddressCategory } from "../shared/AddressCategory";
 
 // 기술자 회원가입
 const SignupTech = () => {
@@ -19,26 +20,6 @@ const SignupTech = () => {
     setError,
     formState: { errors },
   } = useForm();
-
-  const addressCategory = [
-    "서울특별시",
-    "부산광역시",
-    "대구광역시",
-    "인천광역시",
-    "광주광역시",
-    "대전광역시",
-    "울산광역시",
-    "세종특별자치시",
-    "경기도",
-    "강원도",
-    "충청북도",
-    "충청남도",
-    "전라북도",
-    "전라남도",
-    "경상북도",
-    "경상남도",
-    "제주특별자치도",
-  ];
 
   const onValid = (data) => {
     if (data.password !== data.password2) {
@@ -174,7 +155,7 @@ const SignupTech = () => {
             required: "지역을 선택해주세요.",
           })}
         >
-          {addressCategory.map((element, index) => (
+          {AddressCategory.map((element, index) => (
             <option value={element} key={index}>
               {element}
             </option>

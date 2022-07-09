5cc18b98029b0a73b3a6a16e06dc3bea14d0d5a9
nujeyh <dza456@naver.com>
2022-07-09 22:54:47 +0900

    FEAT: 유형 선택 유도 문구 추가

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useMatch } from "react-router-dom";
 import styled from "styled-components";
 
 const Signup = () => {
+  const match = useMatch("/signup");
   return (
     <Wrap>
       <span>회원가입</span>
       <Link to={"/signup/user"}>일반 회원</Link>
       <Link to={"/signup/tech"}>디자이너 / 기술자</Link>
+      {match && <span>회원가입 유형을 선택하세요.</span>}
       <Outlet />
     </Wrap>
   );

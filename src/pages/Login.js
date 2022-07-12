88d659c48797e142a242b21749fb58f2fea1ff73
nujeyh <dza456@naver.com>
2022-07-13 00:25:20 +0900

    FEAT: 로그인 페이지 디자인 적용

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import { loginDB } from "../redux/modules/user";
+import { BlackBtn } from "../elements/Buttons";
+import { HelpText, Input, InputTitle } from "../elements/Inputs";
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -15,58 +17,71 @@ const Login = () => {
   } = useForm();
 
   const onValid = (loginObj) => {
-    dispatch(loginDB(loginObj.username, loginObj.password));
-    // .then((result) => {
-    //   if (!result) {
-    //     setError("loginFail", {
-    //       message: "이메일 또는 비밀번호를 잘못 입력했습니다.",
-    //     });
-    //     return false;
-    //   }
-    //   navigate("/");
-    // });
+    dispatch(loginDB(loginObj.username, loginObj.password)).then((result) => {
+      if (!result) {
+        console.log(result);
+        setError("password", {
+          message: "이메일 또는 비밀번호를 잘못 입력했습니다.",
+        });
+        return false;
+      }
+      navigate("/");
+    });
     console.log(loginObj);
   };
   return (
     <Wrap>
-      <img
-        alt="logo"
-        height="200"
-        src="https://avatars.githubusercontent.com/u/108117705?s=400&u=958d4ffea3ccaa38263d41a4f76cd00b48a47804&v=4"
-      />
-      <Form onSubmit={handleSubmit(onValid)}>
-        <span>이메일</span>
-        <input
+      <h1>RIBBORN</h1>
+      <form onSubmit={handleSubmit(onValid)}>
+        <InputTitle>이메일 주소</InputTitle>
+        <Input
           {...register("username", {
             required: "이메일을 입력해주세요.",
           })}
+          invalid={errors?.username?.message}
           autoComplete="off"
+          placeholder="ribborn@ribborn.co.kr"
         />
-        <span>{errors?.username?.message}</span>
-        <span>비밀번호</span>
-        <input
+        <HelpText> {errors?.username?.message}</HelpText>
+        <InputTitle>비밀번호 입력</InputTitle>
+        <Input
           {...register("password", {
             required: "비밀번호를 입력해주세요.",
           })}
+          invalid={errors?.password?.message}
           type="password"
         />
-        <span>{errors?.password?.message}</span>
-        <span>{errors?.loginFail?.message}</span>
-        <button disabled={errors?.password || errors?.username}>로그인</button>
-      </Form>
-      <Link to="/signup">회원가입</Link>
+        <HelpText>{errors?.password?.message}</HelpText>
+        <ButtonWrap>
+          <BlackBtn disabled={errors?.password || errors?.username}>
+            로그인
+          </BlackBtn>
+        </ButtonWrap>
+      </form>
+      <ButtonWrap>
+        <Link to="/signup">
+          <TextBtn>회원가입</TextBtn>
+        </Link>
+      </ButtonWrap>
     </Wrap>
   );
 };
 const Wrap = styled.div`
-  width: 400px;
-  margin: 50px auto;
-  img {
-    margin: 0 30%;
+  width: 700px;
+  margin: 0 auto 150px auto;
+  h1 {
+    text-align: center;
+    font-size: 30px;
+    font-weight: 700;
+    margin: 90px 0;
   }
 `;
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
+const TextBtn = styled.div`
+  margin-top: 50px;
+  font-size: ${({ theme }) => theme.fontSizes.l};
+`;
+const ButtonWrap = styled.div`
+  margin: 0 auto;
+  width: fit-content;
 `;
 export default Login;

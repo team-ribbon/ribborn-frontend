6c1caecc60c865aab9cff9b7da392feb2ef64e43
treblabin <treblabin@gmail.com>
2022-07-13 20:55:32 +0900

    FIX: 후기게시판 버그 픽스

--- src/page/ReviewDetail.js
+++ src/page/ReviewDetail.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -12,10 +12,12 @@ import PostRightBtn from "../components/PostRightBtn";
 const ReviewDetail = () => {
   const dispatch = useDispatch();
   const params = useParams();
+  const [page, setPage] = useState(0);
   console.log(params);
 
   const post = useSelector((state) => state.post.Post);
   const commentsList = useSelector((state) => state.post.Comments);
+  const userId = useSelector((state) => state.user.user.id);
 
   React.useEffect(() => {
     dispatch(getReviewPostDB(params.postId));
@@ -30,8 +32,19 @@ const ReviewDetail = () => {
   return (
     <Template>
       <PostDetail post={post} />
-      <PostFooter id={params.postId} commentsList={commentsList} />
-      <PostRightBtn noshare={false} id={params.postId} />
+      <PostFooter
+        id={params.postId}
+        commentsList={commentsList}
+        userId={userId}
+        commentCount={post && post.commentCount}
+        page={page}
+      />
+      <PostRightBtn
+        noshare={false}
+        id={params.postId}
+        liked={post && post.liked}
+        likeCount={post && post.likeCount}
+      />
     </Template>
   );
 };

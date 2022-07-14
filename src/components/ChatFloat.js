53b8eca9071a8ec7b8a7c859bbae890413832857
nujeyh <dza456@naver.com>
2022-07-14 19:32:54 +0900

    FEAT: 채팅 플로팅 버튼 디자인 적용

--- src/components/ChatFloat.js
+++ src/components/ChatFloat.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import styled from "styled-components";
+import { SmileChatSVG } from "../elements/SVG";
 import ChatModal from "./ChatModal";
 
 const ChatFloat = () => {
@@ -14,7 +15,14 @@ const ChatFloat = () => {
   return (
     <FloatWrap>
       <Link to="/chat" state={{ backgroundLocation: location }}>
-        {!chatToggle && <button onClick={onClickToggle}>채팅</button>}
+        {!chatToggle && (
+          <ChatButton onClick={onClickToggle}>
+            <div>
+              <SmileChatSVG />
+              <span>채팅</span>
+            </div>
+          </ChatButton>
+        )}
       </Link>
       <ChatWrap>
         <Dim chatToggle={chatToggle} />
@@ -27,8 +35,8 @@ const ChatFloat = () => {
 const FloatWrap = styled.div`
   z-index: 99;
   position: fixed;
-  bottom: 0;
-  right: 0;
+  bottom: 30px;
+  right: 30px;
 `;
 
 const ChatWrap = styled.div`
@@ -37,13 +45,32 @@ const ChatWrap = styled.div`
 
 const Dim = styled.div`
   box-sizing: border-box;
-  display: ${(props) => (props.chatToggle ? "block" : "none")};
+  display: ${({ chatToggle }) => (chatToggle ? "block" : "none")};
   position: fixed;
   top: 0;
   left: 0;
   bottom: 0;
   right: 0;
   background-color: rgba(0, 0, 0, 0.6);
+`;
+
+const ChatButton = styled.div`
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  border: none;
+  background: #ffffff;
+  box-shadow: 4px 4px 25px rgba(0, 25, 72, 0.21);
+  font-size: ${({ theme }) => theme.fontSizes.s};
+  cursor: pointer;
+  div {
+    text-align: center;
+    padding-top: 20px;
+  }
+  span {
+    display: block;
+    padding-top: 5px;
+  }
 `;
 
 export default ChatFloat;

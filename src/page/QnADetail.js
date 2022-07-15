1e04d8440e78c3f0e41d18c19b49a2b51d079b55
treblabin <treblabin@gmail.com>
2022-07-16 01:25:59 +0900

    FEAT: QnA 게시판 무한스크롤

--- src/page/QnADetail.js
+++ src/page/QnADetail.js
@@ -2,8 +2,14 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
 
-import { getQnAPostDB, cleanUpPost } from "../modules/post";
+import {
+  getQnAPostDB,
+  cleanUpPost,
+  loadDoneReset,
+  GetCommentDB,
+} from "../modules/post";
 
 import PostDetail from "../components/PostDetail";
 import PostFooter from "../components/PostFooter";
@@ -13,21 +19,38 @@ const QnADetail = () => {
   const dispatch = useDispatch();
   const params = useParams();
   const [page, setPage] = useState(0);
+  const [loading, setLoading] = React.useState(true);
+
+  const [inViewRef, inView] = useInView();
   console.log(params);
 
   const post = useSelector((state) => state.post.Post);
   const commentsList = useSelector((state) => state.post.Comments);
   const userId = useSelector((state) => state.user.user.id);
+  const loadedEverything = useSelector((state) => state.post.loadedEverything);
 
   React.useEffect(() => {
     dispatch(getQnAPostDB(params.postId));
   }, []);
 
   React.useEffect(() => {
+    if (inView && !loading && !loadedEverything) {
+      setPage(page + 1);
+    }
+  }, [inView, loading]);
+
+  React.useEffect(() => {
     return () => {
       dispatch(cleanUpPost());
     };
   }, []);
+
+  React.useEffect(() => {
+    setLoading(true);
+    dispatch(GetCommentDB(params.postId, page, 5)).then((res) => {
+      setLoading(false);
+    });
+  }, [page]);
 
   return (
     <Template>
@@ -38,6 +61,12 @@ const QnADetail = () => {
         userId={userId}
         commentCount={post && post.commentCount}
         page={page}
+        inViewRef={inViewRef}
+        setPage={setPage}
+        loadDoneReset={loadDoneReset}
+        setLoading={setLoading}
+        inView={inView}
+        loadedEverything={loadedEverything}
       />
       <PostRightBtn
         noshare={true}

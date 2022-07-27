9be9f0c1aae92a015c272e8d4586f804730098c7
treblabin <treblabin@gmail.com>
2022-07-27 23:54:40 +0900

    FEAT: FAQ 상단 바

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -3,10 +3,34 @@ import styled from "styled-components";
 const Faq = () => {
   return (
     <Wrap>
+      <FaqWrap>
+        <Title>자주 묻는 질문</Title>
+        <BoldHR />
+      </FaqWrap>
     </Wrap>
   );
 };
 
 const Wrap = styled.div``;
+
+const FaqWrap = styled.div`
+  max-width: 700px;
+  margin: 40px auto;
+`;
+
+const Title = styled.span`
+  font-weight: 700;
+  font-size: ${({ theme }) => theme.fontSizes.xl};
+  line-height: 36px;
+`;
+
+const BoldHR = styled.div`
+  border-top: 3px solid #000000;
+  margin: 16px 0;
+`;
+
+const HR = styled.div`
+  border-top: 1px solid #afb0b3;
+`;
 
 export default Faq;

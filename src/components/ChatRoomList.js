f4f8aee5c580fa5f9544d2c26b5b1ee4e6430052
nujeyh <dza456@naver.com>
2022-08-02 18:52:27 +0900

    FEAT: 채팅 알림 표시 기능 추가

--- src/components/ChatRoomList.js
+++ src/components/ChatRoomList.js
@@ -22,11 +22,16 @@ const ChatRoomList = ({ location, roomId }) => {
             }}
           >
             <List selected={+room.roomId === +roomId}>
-              <Nickname>{room?.nickname}</Nickname>
-              <Date>{!isExit && moment(room.date).format("HH:mm")}</Date>
-              <Message>
-                {isExit ? "채팅 내역이 없습니다." : room?.message}
-              </Message>
+              <span>
+                <Nickname>{room?.nickname}</Nickname>
+                <Date>{!isExit && moment(room.date).format("HH:mm")}</Date>
+              </span>
+              <span>
+                <Message>
+                  {isExit ? "채팅 내역이 없습니다." : room?.message}
+                </Message>
+                {room?.unreadCnt > 0 && <NotiCount>{room.unreadCnt}</NotiCount>}
+              </span>
             </List>
           </Link>
         );
@@ -36,14 +41,16 @@ const ChatRoomList = ({ location, roomId }) => {
   );
 };
 const List = styled.div`
-  display: flex;
-  flex-flow: wrap;
-  justify-content: space-between;
-  gap: 20px;
   padding: 30px;
   color: ${({ selected }) => selected && "#fff"};
   background-color: ${({ selected, theme }) => selected && theme.colors.black};
   border-bottom: 1px solid ${({ theme }) => theme.colors.gray};
+  span {
+    display: flex;
+    gap: 5px;
+    justify-content: space-between;
+    align-items: center;
+  }
 `;
 const Nickname = styled.div`
   font-size: ${({ theme }) => theme.fontSizes.l};
@@ -51,15 +58,27 @@ const Nickname = styled.div`
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
+  margin-bottom: 20px;
 `;
 const Date = styled.div`
+  margin-bottom: 20px;
   color: ${({ theme }) => theme.colors.gray};
 `;
 const Message = styled.div`
   width: 100%;
-  color: ${({ theme }) => theme.colors.gray};
+  line-height: 21px;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
+  color: ${({ theme }) => theme.colors.gray};
+`;
+const NotiCount = styled.div`
+  width: fit-content;
+  color: #fff;
+  padding: 5px 8px 4px 8px;
+  border-radius: 30px;
+  font-weight: 700;
+  background-color: ${({ theme }) => theme.colors.orange};
+  font-size: ${({ theme }) => theme.fontSizes.s};
 `;
 export default ChatRoomList;

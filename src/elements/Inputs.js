403c11105171cfcb3d16ff15c33079c92b45268f
nujeyh <dza456@naver.com>
2022-08-02 22:00:27 +0900

    FEAT: input padding 조정

--- src/elements/Inputs.js
+++ src/elements/Inputs.js
@@ -7,7 +7,7 @@ export const Input = styled.input`
       invalid ? theme.colors.orange : theme.colors.gray};
   border-radius: 15px;
   padding: ${({ hasCount }) =>
-    hasCount ? "30px 95px 30px 20px" : "25px 20px"};
+    hasCount ? "25px 95px 25px 20px" : "25px 20px"};
   font-size: ${({ theme }) => theme.fontSizes.l};
   &:focus {
     outline: ${({ theme, invalid }) =>
